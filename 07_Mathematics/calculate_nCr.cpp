d4d4eb1666b45aa84e7c57a5cc0ee3a549ec78a7
vasugoel10 <vasugoel1729@gmail.com>
2026-07-04 11:30:38 +0530

    nCr

--- 07_Mathematics/calculate_nCr.cpp
+++ 07_Mathematics/calculate_nCr.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 int nCr(int n,int r){
+    if (n>r) {
     if (r==0){
         return 1;
     }
     else {
         return ((n-r+1)*nCr(n,r-1)/r);
+    }}
+    else {
+        return -1;
     }
 }
 int main(){
